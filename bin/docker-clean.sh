d458dfdd1ecdad5d2f3e02b5976715064b9f2c83
Stick <nstickney@gmail.com>
2018-03-07 19:04:05 -0600

    change: nftables now allows traffic on docker0 both directions

--- bin/docker-clean.sh
+++ bin/docker-clean.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# https://techoverflow.net/2013/10/22/docker-remove-all-images-and-containers/
 
-# Stop all containers
-docker stop "$(docker ps -q)" > /dev/null 2>&1
-
+printf '%s\n' "  Stopping all containers..."
+docker ps -a -q | xargs docker stop > /dev/null 2>&1
 wait
 
-# Delete all containers
+printf '%s\n' "  Removing all containers..."
 yes | docker container prune > /dev/null 2>&1
+wait
 
-# Delete all images
+printf '%s\n' "  Removing all created networks..."
+docker network ls | awk '{print $1 " " $2}' | grep -v -e NETWORK -e bridge -e host -e none | xargs docker network rm > /dev/null 2>&1
+
 if [ "$1" == "all" ]; then
-	docker rmi "$(docker images -q)" /dev/null 2>&1
+	printf '%s\n' "  Removing all images..."
+	docker images -a -q | xargs docker rmi > /dev/null 2>&1
 elif [ "$1" == "built" ]; then
+	printf '%s\n' "  Removing all created images..."
 	docker images -a | grep -v -e / -e REPOSITORY | awk '{print $3}' | xargs docker rmi > /dev/null 2>&1
 fi

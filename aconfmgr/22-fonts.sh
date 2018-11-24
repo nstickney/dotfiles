a7b8f5860fb4e750f06a9a7ce67d17d2bcfea368
Stick <nstickney@pm.me>
2018-11-24 16:24:01 -0500

    rm: unneeded fonts

--- aconfmgr/22-fonts.sh
+++ aconfmgr/22-fonts.sh
@@ -1,8 +1,8 @@
 # shellcheck disable=SC2148
 AddPackage --foreign all-repository-fonts # Meta package for all fonts in the official repositories
-AddPackage --foreign ephifonts # A collection of free fonts including Helvetica, Futura and Gotham (ephifonts.com)
+#AddPackage --foreign ephifonts # A collection of free fonts including Helvetica, Futura and Gotham (ephifonts.com)
 #AddPackage --foreign league-fonts # A package depending on all the fonts from The League Of The Moveable Type
-AddPackage --foreign otf-bitstream-charter # Bitstream charter fonts
-AddPackage --foreign ttf-sil-fonts # A collection of OFL-licensed fonts from SIL (Abyssinica, Andika, Annapurna, Apparatus, Charis and more)
+#AddPackage --foreign otf-bitstream-charter # Bitstream charter fonts
+#AddPackage --foreign ttf-sil-fonts # A collection of OFL-licensed fonts from SIL (Abyssinica, Andika, Annapurna, Apparatus, Charis and more)
 
-CopyFile /etc/fonts/conf.d/20-no-embedded.conf # Don't use bitmaps
+#CopyFile /etc/fonts/conf.d/20-no-embedded.conf # Don't use bitmaps

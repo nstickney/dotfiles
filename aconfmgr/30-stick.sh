51f4ff6ed9cc6b3565dbed18c424226990b2d8de
Stick <nstickney@pm.me>
2018-12-17 11:24:16 -0500

    rm: urxvt (default install)

--- aconfmgr/30-stick.sh
+++ aconfmgr/30-stick.sh
@@ -1,24 +1,25 @@
 AddPackage alacritty # A cross-platform, GPU-accelerated terminal emulator
+AddPackage --foreign exercism-cli # Command line client for exercism.io
+AddPackage --foreign fslint # A utility to find and clean various forms of lint on a filesystem
 AddPackage gramps # Genealogy program, which helps you track your family tree
 AddPackage gtkspell3 # opt dep for gramps
+AddPackage keepassxc # Cross-platform community-driven port of Keepass password manager
+AddPackage --foreign lbry-app-bin # The LBRY app built using electron
 AddPackage libgexiv2 # opt dep for gramps
 AddPackage osm-gps-map # opt dep for gramps
+AddPackage --foreign protonmail-bridge # Application to use IMAP/SMTP with your paid ProtonMail account
 AddPackage python-pyicu # opt dep for gramps
 AddPackage rcs # opt dep for gramps
-AddPackage keepassxc # Cross-platform community-driven port of Keepass password manager
 AddPackage rmlint # Tool to remove duplicates and other lint, being much faster than fdupes
 AddPackage rmlint-shredder # Graphical user interface for rmlint
 AddPackage texlive-bin # opt dep for evince/vim/python
 AddPackage thunderbird # Standalone mail and news reader from mozilla.org
 AddPackage truecrypt # Free open-source cross-platform disk encryption software
-AddPackage rxvt-unicode # Unicode enabled rxvt-clone terminal emulator (urxvt)
-AddPackage urxvt-perls # URL and Mouseless text selection for rxvt-unicode
-AddPackage --foreign urxvt-tabbedex-git # A tabbed extension for rxvt-unicode with several enhancements
+AddPackage --foreign zotero # Zotero Standalone, a free, easy-to-use tool to help you collect, organize, cite, and share your research sources
+
+# AddPackage rxvt-unicode # Unicode enabled rxvt-clone terminal emulator (urxvt)
+# AddPackage urxvt-perls # URL and Mouseless text selection for rxvt-unicode
+# AddPackage --foreign urxvt-tabbedex-git # A tabbed extension for rxvt-unicode with several enhancements
+# AddPackage xpdf # opt dep for Zotero
 # AddPackage zsh # A very advanced and programmable command interpreter (shell) for UNIX
 
-AddPackage --foreign exercism-cli # Command line client for exercism.io
-AddPackage --foreign fslint # A utility to find and clean various forms of lint on a filesystem
-AddPackage --foreign lbry-app-bin # The LBRY app built using electron
-AddPackage --foreign protonmail-bridge # Application to use IMAP/SMTP with your paid ProtonMail account
-AddPackage --foreign zotero # Zotero Standalone, a free, easy-to-use tool to help you collect, organize, cite, and share your research sources
-# AddPackage xpdf # opt dep for Zotero

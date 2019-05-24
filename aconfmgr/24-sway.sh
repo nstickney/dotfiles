c12b443ca253a6b9e6ca19dc5d84b65dfb9a5954
Stick <nstickney@pm.me>
2019-05-23 22:16:19 -0400

    replace rofi with sway-launcher

--- aconfmgr/24-sway.sh
+++ aconfmgr/24-sway.sh
@@ -6,7 +6,6 @@ AddPackage swaylock # Screen locker for Wayland
 AddPackage grim # Screenshot utility that works under Wayland
 AddPackage slurp # Select a region in a Wayland compositor
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
-AddPackage rofi # Popup window switcher roughly based on superswitcher, requiring only xlib and xft
 AddPackage xdg-utils # Command line tools that assist applications with a variety of desktop integration tasks
 
 AddPackage --foreign i3status-rust-git # Very resourcefriendly and feature-rich replacement for i3status, written in pure Rust

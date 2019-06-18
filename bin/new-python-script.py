46166364f8266f92fb16616eb82535415a1c606f
Stick <nstickney@pm.me>
2019-06-18 13:53:39 -0400

    add argparse to new-python-script.py

--- bin/new-python-script.py
+++ bin/new-python-script.py
@@ -1,38 +1,57 @@
 #!/usr/bin/env python
-'''Create a new executable python script'''
+''' Create a new executable python script '''
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
 __author__ = 'Stick'
-__email__ = 'stick@stma.is'
 __credits__ = ['https://www.python-boilerplate.com/']
+__email__ = 'stick@stma.is'
 __license__ = 'Mozilla PL'
+__version__ = '0.2.0'
 
+from argparse import ArgumentParser
 from os import chmod
 from stat import S_IRUSR, S_IWUSR, S_IXUSR
-from sys import argv, stderr
 
 
-def main(filename):
-    '''Main entry point of the script'''
-    if filename[-3:] != '.py':
-        filename += '.py'
-    with open(filename, 'w') as f:
+def main(args):
+    ''' Main entry point of the script '''
+    if args.filename[-3:] != '.py':
+        args.filename += '.py'
+    with open(args.filename, 'w') as f:
         with open(__file__, 'r') as s:
             f.write(s.read())
-    chmod(filename, S_IRUSR | S_IWUSR | S_IXUSR)
-
-
-def usage():
-    '''Print the script's usage message'''
-    message = 'Usage: [python[3]] ' + argv[0] + ' <filename>\n'
-    stderr.write(message)
-    exit(1)
+    chmod(args.filename, S_IRUSR | S_IWUSR | S_IXUSR)
 
 
 if __name__ == '__main__':
-    if len(argv) != 2 or '-h' in argv or '--help' in argv:
-        usage()
-    main(argv[1])
+    ''' Executed when the script is run directly '''
+    parser = ArgumentParser()
+
+    # Required positional argument
+    parser.add_argument('filename', help='filename of new script')
+
+    # Optional argument flag which defaults to False
+    # parser.add_argument('-f', '--flag', action='store_true', default=False)
+
+    # Optional argument which requires a parameter (eg. -d test)
+    # parser.add_argument('-n', '--name', action='store', dest='name')
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    # parser.add_argument(
+    #     '-v',
+    #     '--verbose',
+    #     action='count',
+    #     default=0,
+    #     help='output verbosity (-v, -vv, etc)')
+
+    # Specify output of '--version'
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='%(prog)s (version {version})'.format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)

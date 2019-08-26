59389a5a2c91385e7a7e803a3a3df0dda7d731d1
Stick <stick@stma.is>
2019-08-26 11:11:52 -0400

    refactor dev necessities

--- aconfmgr/25-dev.sh
+++ aconfmgr/25-dev.sh
@@ -9,6 +9,7 @@ AddPackage git # the fast distributed version control system
 AddPackage git-crypt # Transparent file encryption in Git
 AddPackage hyperfine # A command-line benchmarking tool
 AddPackage namcap # A Pacman package analyzer
+AddPackage nmap # Utility for network discovery and security auditing
 AddPackage parallel # A shell tool for executing jobs in parallel
 AddPackage peda # Python Exploit Development Assistance for GDB
 AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
@@ -23,62 +24,61 @@ AddPackage vim-spell-en # Language files for Vim spell checking
 # Remote admin
 if [ "$USER" == 'stick' ]; then
 	AddPackage terraform # Tool for building, changing, and versioning infrastructure safely and efficiently
+
+	# Network Testing
+	AddPackage gnu-netcat # GNU rewrite of netcat, the network piping application
+	AddPackage ipguard # arp<->ip relation checking tool
+	AddPackage vegeta # HTTP load testing tool
+
+	# Programming Languages
+
+	### Bash
+	AddPackage bash-bats # Bash Automated Testing System
+	AddPackage checkbashisms # Debian script that checks for bashisms
+	AddPackage shellcheck # Shell script analysis tool
+	AddPackage shellharden # Bash linter and syntax highlighter
+
+	### C/C++
+	AddPackage cmake # A cross-platform open-source make system
+	AddPackage cppcheck # A tool for static C/C++ code analysis
+	AddPackage flawfinder # Searches through source code for potential security flaws
+	AddPackage vim-omnicppcomplete # vim c++ completion omnifunc with a ctags database
+
+	### Go
+	AddPackage delve # A debugger for the Go programming language.
+	AddPackage go # Core compiler tools for the Go programming language
+
+	### Java
+	AddPackage maven # Java project management and project comprehension tool
+	AddPackage vim-jad # Automatically decompile Java class files and display Java code
+
+	### Javascript
+	AddPackage npm # A package manager for javascript
+
+	### HTML
+	AddPackage tidy # A tool to tidy down your HTML code to a clean style
+
+	### Multi-language
+	AddPackage astyle # A free, fast and small automatic formatter for C, C++, C#, and Java source code.
+	AddPackage eslint # An AST-based pattern checker for JavaScript
+	AddPackage prettier # An opinionated code formatter for JS, JSON, CSS, YAML and much more
+
+	### Perl
+	AddPackage perl-tidy # Parses and beautifies perl source
+
+	### Python
+	AddPackage autopep8 # A tool that automatically formats Python code to conform to the PEP 8 style guide
+	AddPackage flake8 # The modular source code checker: pep8, pyflakes and co
+	AddPackage python-pip # The PyPA recommended tool for installing Python packages
+	AddPackage python-pycodestyle # Python style guide checker
+	AddPackage python-pytest # Simple powerful testing with Python
+	AddPackage tk # A windowing toolkit for use with tcl (python turtles)
+	AddPackage vim-jedi # Vim plugin for jedi, an awesome Python autocompletion
+	AddPackage yapf # Python style guide checker
+
+	### Rust
+	AddPackage rust # Systems programming language focused on safety, speed and concurrency
+
+	### Scala
+	AddPackage sbt # The interactive build tool
 fi
-
-# Network Testing
-AddPackage gnu-netcat # GNU rewrite of netcat, the network piping application
-AddPackage ipguard # arp<->ip relation checking tool
-AddPackage nmap # Utility for network discovery and security auditing
-AddPackage vegeta # HTTP load testing tool
-
-# Programming Languages
-
-### Bash
-AddPackage bash-bats # Bash Automated Testing System
-AddPackage checkbashisms # Debian script that checks for bashisms
-AddPackage shellcheck # Shell script analysis tool
-AddPackage shellharden # Bash linter and syntax highlighter
-
-### C/C++
-AddPackage cmake # A cross-platform open-source make system
-AddPackage cppcheck # A tool for static C/C++ code analysis
-AddPackage flawfinder # Searches through source code for potential security flaws
-AddPackage vim-omnicppcomplete # vim c++ completion omnifunc with a ctags database
-
-### Go
-AddPackage delve # A debugger for the Go programming language.
-AddPackage go # Core compiler tools for the Go programming language
-
-### Java
-AddPackage maven # Java project management and project comprehension tool
-AddPackage vim-jad # Automatically decompile Java class files and display Java code
-
-### Javascript
-AddPackage npm # A package manager for javascript
-
-### HTML
-AddPackage tidy # A tool to tidy down your HTML code to a clean style
-
-### Multi-language
-AddPackage astyle # A free, fast and small automatic formatter for C, C++, C#, and Java source code.
-AddPackage eslint # An AST-based pattern checker for JavaScript
-AddPackage prettier # An opinionated code formatter for JS, JSON, CSS, YAML and much more
-
-### Perl
-AddPackage perl-tidy # Parses and beautifies perl source
-
-### Python
-AddPackage autopep8 # A tool that automatically formats Python code to conform to the PEP 8 style guide
-AddPackage flake8 # The modular source code checker: pep8, pyflakes and co
-AddPackage python-pip # The PyPA recommended tool for installing Python packages
-AddPackage python-pycodestyle # Python style guide checker
-AddPackage python-pytest # Simple powerful testing with Python
-AddPackage tk # A windowing toolkit for use with tcl (python turtles)
-AddPackage vim-jedi # Vim plugin for jedi, an awesome Python autocompletion
-AddPackage yapf # Python style guide checker
-
-### Rust
-AddPackage rust # Systems programming language focused on safety, speed and concurrency
-
-### Scala
-# AddPackage sbt # The interactive build tool

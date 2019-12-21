95b5d5339f87ba43edc6d3c316c5f05cffb8832c
Stick <stick@stma.is>
2019-12-20 22:32:38 -0500

    remove git_template (ctags, etc)

--- .config/aconfmgr/25-dev.sh
+++ .config/aconfmgr/25-dev.sh
@@ -2,7 +2,6 @@
 AddPackage bat # Cat clone with syntax highlighting and git integration
 AddPackage cloc # Count lines of code
 AddPackage code # The Open Source build of Visual Studio Code (vscode) editor
-AddPackage ctags # Generates an index file of language objects found in source files
 AddPackage --foreign eva # simple calculator REPL, similar to bc(1)
 AddPackage fd # Simple, fast and user-friendly alternative to find
 AddPackage fzf # Command-line fuzzy finder

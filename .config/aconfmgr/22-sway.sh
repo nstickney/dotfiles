0d8890a5c6a509edbf30bb08a77808239acab9db
Stick <stick@stma.is>
2020-01-02 09:54:17 -0500

    add mako (notifications)

--- .config/aconfmgr/22-sway.sh
+++ .config/aconfmgr/22-sway.sh
@@ -12,6 +12,7 @@ AddPackage grim # Screenshot utility that works under Wayland
 AddPackage --foreign i3status-rust-git # Very resourcefriendly and feature-rich replacement for i3status, written in pure Rust
 AddPackage pacman-contrib # used in i3status-rust-git config
 AddPackage imv # Image viewer for Wayland and X11
+AddPackage mako # Lightweight notification daemon for Wayland
 AddPackage --foreign network-manager-applet-indicator # Applet for managing network connections, with Appindicator/StatusNotifierItem support from GNOME GitLab
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
 AddPackage python-i3ipc # An improved Python library to control i3wm
@@ -19,4 +20,3 @@ AddPackage slurp # Select a region in a Wayland compositor
 AddPackage wl-clipboard # Command-line copy/paste utilities for Wayland
 AddPackage xdg-utils # Command line tools that assist applications with a variety of desktop integration tasks
 AddPackage xorg-xev # Print contents of X events
-

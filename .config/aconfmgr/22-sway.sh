0aeef1bc2b97925bd9489b2df67a9d4874e1a504
Stick <stick@stma.is>
2020-03-16 22:30:14 -0400

    switch from i3status-rs to waybar

--- .config/aconfmgr/22-sway.sh
+++ .config/aconfmgr/22-sway.sh
@@ -1,20 +1,20 @@
-AddPackage sway # Tiling Wayland compositor and replacement for the i3 window manager
+AddPackage sway     # Tiling Wayland compositor and replacement for the i3 window manager
 AddPackage swayidle # Idle management daemon for Wayland
 AddPackage swaylock # Screen locker for Wayland
-AddPackage wmname # A utility to set the name of your window manager
+AddPackage wmname   # A utility to set the name of your window manager
 
-AddPackage alacritty # A cross-platform, GPU-accelerated terminal emulator
-AddPackage alsa-utils # Opt dep for i3status-rust-git
-AddPackage grim # Screenshot utility that works under Wayland
-AddPackage --foreign i3status-rust-git # Very resourcefriendly and feature-rich replacement for i3status, written in pure Rust
-AddPackage pacman-contrib # used in i3status-rust-git config
-AddPackage imv # Image viewer for Wayland and X11
-AddPackage mako # Lightweight notification daemon for Wayland
+AddPackage alacritty                                  # A cross-platform, GPU-accelerated terminal emulator
+AddPackage alsa-utils                                 # Opt dep for i3status-rust-git
+AddPackage grim                                       # Screenshot utility that works under Wayland
+AddPackage pacman-contrib                             # used in i3status-rust-git config
+AddPackage imv                                        # Image viewer for Wayland and X11
+AddPackage mako                                       # Lightweight notification daemon for Wayland
 AddPackage --foreign network-manager-applet-indicator # Applet for managing network connections, with Appindicator/StatusNotifierItem support from GNOME GitLab
-AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
-AddPackage python-i3ipc # An improved Python library to control i3wm
-AddPackage slurp # Select a region in a Wayland compositor
-AddPackage --foreign wdisplays-git # GUI display configurator for wlroots compositors
-AddPackage wl-clipboard # Command-line copy/paste utilities for Wayland
-AddPackage xdg-utils # Command line tools that assist applications with a variety of desktop integration tasks
-AddPackage xorg-xev # Print contents of X events
+AddPackage playerctl                                  # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
+AddPackage python-i3ipc                               # An improved Python library to control i3wm
+AddPackage slurp                                      # Select a region in a Wayland compositor
+AddPackage waybar                                     # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage --foreign wdisplays-git                    # GUI display configurator for wlroots compositors
+AddPackage wl-clipboard                               # Command-line copy/paste utilities for Wayland
+AddPackage xdg-utils                                  # Command line tools that assist applications with a variety of desktop integration tasks
+AddPackage xorg-xev                                   # Print contents of X events

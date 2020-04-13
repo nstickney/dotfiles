88ba26d2a6663a2e2f5047d6c5545878a5178e48
Stick <stick@stma.is>
2020-04-13 09:56:35 -0400

    add asciidoc + related to aconfmgr

--- .config/aconfmgr/30-stick.sh
+++ .config/aconfmgr/30-stick.sh
@@ -1,25 +1,28 @@
-AddPackage --foreign bitwarden-bin # A secure and free password manager for all of your devices.
-AddPackage boinc                   # Berkeley Open Infrastructure for Network Computing for desktop
-AddPackage calibre                 # Ebook management application (python2 build)
-AddPackage --foreign exercism-bin  # Command line client for exercism.io
-AddPackage hugo                    # Fast and Flexible Static Site Generator in Go
-AddPackage --foreign hydroxide-git # A third-party, open-source ProtonMail CardDAV, IMAP and SMTP bridge
-AddPackage jp2a                    # A small utility for converting JPG images to ASCII
-AddPackage kbfs                    # The Keybase filesystem
-AddPackage keybase                 # CLI tool for GPG with keybase.io
-AddPackage keybase-gui             # GUI frontend for GPG with keybase.io
-AddPackage libreoffice-fresh       # LibreOffice branch which contains new features and program enhancements
-AddPackage coin-or-mp              # libreoffice-fresh opt dep
-AddPackage pstoedit                # libreoffice-fresh opt dep
-AddPackage ncdu                    # Disk usage analyzer with an ncurses interface
-AddPackage pandoc                  # Conversion between markup formats
-AddPackage pandoc-citeproc         # Supports using pandoc with citeproc (pandoc opt dep)
-AddPackage pandoc-crossref         # Pandoc filter for cross-references (pandoc opt dep)
-AddPackage rmlint                  # Tool to remove duplicates and other lint, being much faster than fdupes
-AddPackage rmlint-shredder         # Graphical user interface for rmlint
-AddPackage texlive-fontsextra      # TeX Live - all sorts of extra fonts
-AddPackage texlive-latexextra      # TeX Live - Large collection of add-on packages for LaTeX
-AddPackage truecrypt               # Free open-source cross-platform disk encryption software
+AddPackage --foreign bitwarden-bin           # A secure and free password manager for all of your devices.
+AddPackage boinc                             # Berkeley Open Infrastructure for Network Computing for desktop
+AddPackage calibre                           # Ebook management application (python2 build)
+AddPackage --foreign exercism-bin            # Command line client for exercism.io
+AddPackage hugo                              # Fast and Flexible Static Site Generator in Go
+AddPackage asciidoctor                       # used with hugo
+AddPackage ruby-rouge                        # used with hugo/asciidoctor
+AddPackage --foreign ruby-asciidoctor-html5s # used with hugo/asciidoctor
+AddPackage --foreign hydroxide-git           # A third-party, open-source ProtonMail CardDAV, IMAP and SMTP bridge
+AddPackage jp2a                              # A small utility for converting JPG images to ASCII
+AddPackage kbfs                              # The Keybase filesystem
+AddPackage keybase                           # CLI tool for GPG with keybase.io
+AddPackage keybase-gui                       # GUI frontend for GPG with keybase.io
+AddPackage libreoffice-fresh                 # LibreOffice branch which contains new features and program enhancements
+AddPackage coin-or-mp                        # libreoffice-fresh opt dep
+AddPackage pstoedit                          # libreoffice-fresh opt dep
+AddPackage ncdu                              # Disk usage analyzer with an ncurses interface
+AddPackage pandoc                            # Conversion between markup formats
+AddPackage pandoc-citeproc                   # Supports using pandoc with citeproc (pandoc opt dep)
+AddPackage pandoc-crossref                   # Pandoc filter for cross-references (pandoc opt dep)
+AddPackage rmlint                            # Tool to remove duplicates and other lint, being much faster than fdupes
+AddPackage rmlint-shredder                   # Graphical user interface for rmlint
+AddPackage texlive-fontsextra                # TeX Live - all sorts of extra fonts
+AddPackage texlive-latexextra                # TeX Live - Large collection of add-on packages for LaTeX
+AddPackage truecrypt                         # Free open-source cross-platform disk encryption software
 
 # AddPackage --foreign fslint # A utility to find and clean various forms of lint on a filesystem
 # AddPackage gramps # Genealogy program, which helps you track your family tree

9e5d27bd3c07c77e6391fbc4e7bb3b5f50d53607
Stick <stick@stma.is>
2020-11-09 21:57:36 -0500

    push sway-launcher-desktop to package manager

--- .config/aconfmgr/22-sway.sh
+++ .config/aconfmgr/22-sway.sh
@@ -3,19 +3,20 @@ AddPackage swayidle # Idle management daemon for Wayland
 AddPackage swaylock # Screen locker for Wayland
 AddPackage wmname   # A utility to set the name of your window manager
 
-AddPackage alacritty               # A cross-platform, GPU-accelerated terminal emulator
-AddPackage grim                    # Screenshot utility that works under Wayland
-AddPackage pacman-contrib          # used in i3status-rust-git config
-AddPackage imv                     # Image viewer for Wayland and X11
-AddPackage mako                    # Lightweight notification daemon for Wayland
-AddPackage playerctl               # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
-AddPackage python-i3ipc            # An improved Python library to control i3wm
-AddPackage slurp                   # Select a region in a Wayland compositor
-AddPackage ttf-nerd-fonts-symbols  # sway-launcher-desktop opt dep
-AddPackage waybar                  # Highly customizable Wayland bar for Sway and Wlroots based compositors
-AddPackage --foreign wdisplays-git # GUI display configurator for wlroots compositors
-AddPackage --foreign wev-git       # Print wayland events, like xev(1)
-AddPackage wl-clipboard            # Command-line copy/paste utilities for Wayland
-AddPackage wf-recorder             # Screen recorder for wlroots-based compositors such as sway
-AddPackage --foreign wlsunset-git  # Day/night gamma adjustments for Wayland
-AddPackage xdg-utils               # Command line tools that assist applications with a variety of desktop integration tasks
+AddPackage alacritty                       # A cross-platform, GPU-accelerated terminal emulator
+AddPackage grim                            # Screenshot utility that works under Wayland
+AddPackage pacman-contrib                  # used in i3status-rust-git config
+AddPackage imv                             # Image viewer for Wayland and X11
+AddPackage mako                            # Lightweight notification daemon for Wayland
+AddPackage playerctl                       # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
+AddPackage python-i3ipc                    # An improved Python library to control i3wm
+AddPackage slurp                           # Select a region in a Wayland compositor
+AddPackage --foreign sway-launcher-desktop # TUI Application launcher with Desktop Entry support.
+AddPackage ttf-nerd-fonts-symbols          # sway-launcher-desktop opt dep
+AddPackage waybar                          # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage --foreign wdisplays-git         # GUI display configurator for wlroots compositors
+AddPackage --foreign wev-git               # Print wayland events, like xev(1)
+AddPackage wl-clipboard                    # Command-line copy/paste utilities for Wayland
+AddPackage wf-recorder                     # Screen recorder for wlroots-based compositors such as sway
+AddPackage --foreign wlsunset-git          # Day/night gamma adjustments for Wayland
+AddPackage xdg-utils                       # Command line tools that assist applications with a variety of desktop integration tasks

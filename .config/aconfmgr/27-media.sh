99e4f8d677e0f84acbca756bdbb11c162620e234
Stick <stick@stma.is>
2020-11-23 21:33:37 -0500

    improve git precommit image optimization (gifsicle)

--- .config/aconfmgr/27-media.sh
+++ .config/aconfmgr/27-media.sh
@@ -1,20 +1,21 @@
-AddPackage alsa-utils # An alternative implementation of Linux sound support
-AddPackage asciinema # Record and share terminal sessions
-AddPackage audacity # A program that lets you manipulate digital audio waveforms
-AddPackage gimp # GNU Image Manipulation Program
-AddPackage gst-libav # Gstreamer libav Plugin
-AddPackage gst-plugins-bad # GStreamer Multimedia Framework Bad Plugins
+AddPackage alsa-utils       # An alternative implementation of Linux sound support
+AddPackage asciinema        # Record and share terminal sessions
+AddPackage audacity         # A program that lets you manipulate digital audio waveforms
+AddPackage gifsicle         # A powerful command-line program for creating, editing, manipulating and getting information about GIF images and animations
+AddPackage gimp             # GNU Image Manipulation Program
+AddPackage gst-libav        # Gstreamer libav Plugin
+AddPackage gst-plugins-bad  # GStreamer Multimedia Framework Bad Plugins
 AddPackage gst-plugins-ugly # GStreamer Multimedia Framework Ugly Plugins
-AddPackage jpegoptim # Jpeg optimisation utility
-AddPackage handbrake # Multithreaded video transcoder
-AddPackage inkscape # Professional vector graphics editor
-AddPackage libdvdcss # Portable abstraction library for DVD decryption
-AddPackage openshot # an open-source, non-linear video editor for Linux based on MLT framework
-AddPackage optipng # Compresses PNG files to a smaller size, without losing any information.
-AddPackage pavucontrol # PulseAudio Volume Control
-AddPackage pulseaudio # A featureful, general-purpose sound server
-AddPackage svgcleaner # An application that cleans svg images from unnecessary data in batch mode.
-AddPackage vlc # A multi-platform MPEG, VCD/DVD, and DivX player
+AddPackage jpegoptim        # Jpeg optimisation utility
+AddPackage handbrake        # Multithreaded video transcoder
+AddPackage inkscape         # Professional vector graphics editor
+AddPackage libdvdcss        # Portable abstraction library for DVD decryption
+AddPackage openshot         # an open-source, non-linear video editor for Linux based on MLT framework
+AddPackage optipng          # Compresses PNG files to a smaller size, without losing any information.
+AddPackage pavucontrol      # PulseAudio Volume Control
+AddPackage pulseaudio       # A featureful, general-purpose sound server
+AddPackage svgcleaner       # An application that cleans svg images from unnecessary data in batch mode.
+AddPackage vlc              # A multi-platform MPEG, VCD/DVD, and DivX player
 
 CopyFile /etc/pulse/default.pa
 CreateLink /etc/systemd/user/sockets.target.wants/pipewire.socket /usr/lib/systemd/user/pipewire.socket

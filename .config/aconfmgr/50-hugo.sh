31601f997bdb99e85872600511f49667b7d0cc1a
Stick <stick@stma.is>
2025-11-11 16:48:40 -0500

    asciidoctor html5 is broken...skip for now

--- .config/aconfmgr/50-hugo.sh
+++ .config/aconfmgr/50-hugo.sh
@@ -2,5 +2,5 @@ if ! DetectVM; then                           # Don't install except on actual w
 	AddPackage asciidoctor                       # An implementation of AsciiDoc in Ruby
 	AddPackage hugo                              # Fast and Flexible Static Site Generator in Go
 	AddPackage ruby-rouge                        # Pure-ruby code highlighter that is compatible with pygments
-	AddPackage --foreign ruby-asciidoctor-html5s # Semantic HTML5 converter (backend) for Asciidoctor
+	# AddPackage --foreign ruby-asciidoctor-html5s # Semantic HTML5 converter (backend) for Asciidoctor
 fi
